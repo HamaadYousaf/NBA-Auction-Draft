ca5d8523c6b506a88f25539f50acba5ab18a0637
HamaadYousaf <hamaaadyousaf@outlook.com>
2024-01-08 18:57:44 -0500

    refactored feed

--- server/middleware/socket.js
+++ server/middleware/socket.js
@@ -1,33 +1,42 @@
 import { draftTimer } from '../utils/draftUtils.js';
 import { sessionMiddleware } from './session.js';
+import moment from 'moment';
 
 export const socketMiddleware = (io) => {
     io.engine.use(sessionMiddleware);
+    let user;
 
     io.on('connection', socket => {
-        socket.on('joined-room', (room) => {
-            socket.join('draft-room');
-            socket.broadcast.to('draft-room').emit('feed', `${socket.id} has joined the draft room`, '3:00PM');
-            socket.broadcast.to('draft-room').emit('user-joined');
+        socket.on('joined-room', (room, username) => {
+            user = username;
+            if (user) {
+                socket.join('draft-room');
+                socket.broadcast.to('draft-room').emit('feed', `${user} has joined the draft room`, moment().format('h:mm a'));
+                socket.broadcast.to('draft-room').emit('user-joined');
+            }
         });
 
         socket.on('start-timer', () => {
             socket.broadcast.to('draft-room').emit('run-draft');
-            io.to('draft-room').emit('feed', 'The host has started the draft', '3:00PM');
+            io.to('draft-room').emit('feed', 'The host has started the draft', moment().format('h:mm a'));
             draftTimer(socket, io);
         });
 
         socket.on('disconnect', () => {
-            socket.leave('draft-room');
-            socket.broadcast.to('draft-room').emit('feed', `${socket.id} has left the draft room`, '3:00PM');
-            socket.removeAllListeners();
+            if (user) {
+                socket.leave('draft-room');
+                socket.broadcast.to('draft-room').emit('user-joined');
+                socket.removeAllListeners();
+            }
         })
 
         socket.on('leave-room', () => {
-            socket.leave('draft-room');
-            socket.broadcast.to('draft-room').emit('feed', `${socket.id} has left the draft room`, '3:00PM');
-            socket.broadcast.to('draft-room').emit('user-joined');
-            socket.removeAllListeners();
+            if (user) {
+                socket.leave('draft-room');
+                socket.broadcast.to('draft-room').emit('feed', `${user} has left the draft room`, moment().format('h:mm a'));
+                socket.broadcast.to('draft-room').emit('user-joined');
+                socket.removeAllListeners();
+            }
         })
     })
 }
